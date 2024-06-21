081f53f0560acc3df1230250bcaa881a3081d362
Angel01w <penaa3114@gmail.com>
2024-06-20 20:09:53 -0400

    Login

--- Nominas/Nominas/obj/Release/net8.0/Nominas.RazorAssemblyInfo.cs
+++ Nominas/Nominas/obj/Release/net8.0/Nominas.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Nominas")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcd9f927001fd956469d87dc282dbe19820df4de")]
-[assembly: System.Reflection.AssemblyProductAttribute("Nominas")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Nominas")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
