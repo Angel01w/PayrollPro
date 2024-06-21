8190fe1c49d55fe68b1fe155bc75683af72406e9
Jnicole10 <94860050+Jnicole10@users.noreply.github.com>
2024-06-21 10:24:15 -0400

    PayrollPro.sql

--- Base de datos/PayrollPro.sql
+++ Base de datos/PayrollPro.sql
@@ -16,18 +16,18 @@ CREATE TABLE [dbo].[Departamentos](
 GO
 
 INSERT INTO [dbo].[Departamentos] (Nombre, Ubicacion, Responsable_Area) VALUES
-('Recursos Humanos', 'Piso 1', 'Patricia EncarnaciÛn'),
-('Finanzas', 'Piso 3', 'Jorge BisonÛ'),
+('Recursos Humanos', 'Piso 1', 'Patricia Encarnaci√≥n'),
+('Finanzas', 'Piso 3', 'Jorge Bison√≥'),
 ('Marketing', 'Piso 3', 'Laura Paredes'),
 ('Ventas', 'Piso 3', 'Carlos Casals'),
-('AtenciÛn al Cliente', 'Piso 1', 'Paula S·nchez'),
-('TecnologÌa', 'Piso 4', 'Juan Contreras'),
-('LogÌstica', 'Piso 4', 'Angel JimÈnez'),
-('ProducciÛn', 'Piso 2', 'Luis RodrÌguez'),
-('Compras', 'Piso 2', 'SofÌa Hern·ndez'),
-('AdministraciÛn', 'Piso 4', 'Miguel Santos'),
-('ComunicaciÛn', 'Piso 3', 'Amelia Moreno'),
-('PlanificaciÛn', 'Piso 4', 'Jorge Ortega');
+('Atenci√≥n al Cliente', 'Piso 1', 'Paula S√°nchez'),
+('Tecnolog√≠a', 'Piso 4', 'Juan Contreras'),
+('Log√≠stica', 'Piso 4', 'Angel Jim√©nez'),
+('Producci√≥n', 'Piso 2', 'Luis Rodr√≠guez'),
+('Compras', 'Piso 2', 'Sof√≠a Hern√°ndez'),
+('Administraci√≥n', 'Piso 4', 'Miguel Santos'),
+('Comunicaci√≥n', 'Piso 3', 'Amelia Moreno'),
+('Planificaci√≥n', 'Piso 4', 'Jorge Ortega');
 
 SELECT * FROM [dbo].[Departamentos]
 
@@ -50,11 +50,11 @@ VALUES
 ('001-1789632-5', 'Jorge Mesa', 6, 4, 55000, 1),
 ('402-3547895-8', 'Lia Batista', 5, 11, 38000, 1),
 ('402-7459815-2', 'Eduardo Gomez', 6, 12, 40000, 1),
-('002-1019682-1', 'Viana SalomÛn', 6, 4, 40000, 8),
+('002-1019682-1', 'Viana Salom√≥n', 6, 4, 40000, 8),
 ('402-7804198-6', 'Camila Vargas', 3, 3, 55000, 1),
 ('402-3033972-3', 'Diana Moreno', 10, 10, 38000, 1),
-('003-3458896-5', 'Cristian CedeÒo', 12, 13, 60000, 11),
-('001-1277634-4', 'Frank EncarnaciÛn', 8, 8, 45000, 2);
+('003-3458896-5', 'Cristian Cede√±o', 12, 13, 60000, 11),
+('001-1277634-4', 'Frank Encarnaci√≥n', 8, 8, 45000, 2);
 
 SELECT * FROM [dbo].[Empleados]
 
@@ -76,14 +76,14 @@ VALUES
 ('Especialista en Marketing', 'Bajo', 40000, 70000),
 ('Desarrollador de Software', 'Medio', 45000, 90000),
 ('Ejecutivo de Ventas', 'Bajo', 40000, 68000),
-('Jefe de LogÌstica', 'Medio', 50000, 80000),
+('Jefe de Log√≠stica', 'Medio', 50000, 80000),
 ('Comprador', 'Bajo', 42000, 70000),
-('Supervisor de ProducciÛn', 'Alto', 48000, 78000),
+('Supervisor de Producci√≥n', 'Alto', 48000, 78000),
 ('Investigador', 'Alto', 55000, 90000),
 ('Asistente Administrativo', 'Bajo', 38000, 62000),
 ('Representante de Servicio al Cliente', 'Bajo', 35000, 60000),
-('TÈcnico de Mantenimiento', 'Medio', 40000, 68000),
-('Especialista en InnovaciÛn', 'Alto', 53000, 90000);
+('T√©cnico de Mantenimiento', 'Medio', 40000, 68000),
+('Especialista en Innovaci√≥n', 'Alto', 53000, 90000);
 
 SELECT * FROM [dbo].[Puestos]
 
@@ -118,11 +118,11 @@ VALUES
 (6, 3, 'Bono', '2023-08-15', 4500, 'Completado'),
 (7, 10, 'Bono', '2023-08-31', 3000, 'Completado'),
 (9, 2, 'Bono', '2023-09-30', 3500, 'Completado'),
-(1, 1, 'ComisiÛn', '2023-10-15', 2000, 'Completado'),
-(4, 4, 'ComisiÛn', '2023-11-30', 1200, 'Completado'),
-(6, 3, 'ComisiÛn', '2023-12-31', 1500, 'Completado'),
-(8, 12, 'ComisiÛn', '2024-01-31', 2000, 'Completado'),
-(9, 2, 'ComisiÛn', '2024-02-15', 1200, 'Completado');
+(1, 1, 'Comisi√≥n', '2023-10-15', 2000, 'Completado'),
+(4, 4, 'Comisi√≥n', '2023-11-30', 1200, 'Completado'),
+(6, 3, 'Comisi√≥n', '2023-12-31', 1500, 'Completado'),
+(8, 12, 'Comisi√≥n', '2024-01-31', 2000, 'Completado'),
+(9, 2, 'Comisi√≥n', '2024-02-15', 1200, 'Completado');
 
 SELECT * FROM [dbo].[Registro Transacciones]
 
@@ -138,19 +138,19 @@ GO
 
 INSERT INTO [dbo].[Tipos de deducciones] (Id, Descripcion, Tipo_Descuentos, Estado)
 VALUES
-(1, 'Seguro MÈdico', 'Fijo', 'Activo'),
+(1, 'Seguro M√©dico', 'Fijo', 'Activo'),
 (2, 'Impuesto Sobre la Renta', 'Variable', 'Activo'),
-(3, 'Aporte de PensiÛn', 'Fijo', 'Activo'),
+(3, 'Aporte de Pensi√≥n', 'Fijo', 'Activo'),
 (4, 'Seguro de Vida', 'Fijo', 'Activo'),
-(5, 'PrÈstamo', 'Variable', 'Activo'),
+(5, 'Pr√©stamo', 'Variable', 'Activo'),
 (6, 'Cuota Sindical', 'Fijo', 'Activo'),
 (7, 'Fondo de Ahorro', 'Variable', 'Inactivo'),
 (8, 'Multa', 'Variable', 'Inactivo'),
 (9, 'Adelanto de Sueldo', 'Variable', 'Activo'),
-(10, 'Gastos MÈdicos', 'Fijo', 'Activo'),
-(11, 'Seguro de AutomÛvil', 'Fijo', 'Activo'),
+(10, 'Gastos M√©dicos', 'Fijo', 'Activo'),
+(11, 'Seguro de Autom√≥vil', 'Fijo', 'Activo'),
 (12, 'Gastos de Viaje', 'Variable', 'Activo'),
-(13, 'Gastos de CapacitaciÛn', 'Variable', 'Activo');
+(13, 'Gastos de Capacitaci√≥n', 'Variable', 'Activo');
 
 SELECT * FROM [dbo].[Tipos de deducciones]
 
@@ -164,23 +164,22 @@ CREATE TABLE [dbo].[Tipos de ingreso](
 ) ON [PRIMARY]
 GO
 
-
 INSERT INTO [dbo].[Tipos de ingreso] (Descripcion, Tipo_Salario, Estado)
 VALUES
 ('Salario Base', 'Fijo', 'Activo'),
 ('Bono', 'Fijo', 'Activo'),
-('ComisiÛn', 'Variable', 'Activo'),
+('Comisi√≥n', 'Variable', 'Activo'),
 ('Horas Extra', 'Fijo', 'Activo'),
 ('Incentivo', 'Variable', 'Activo'),
-('Bono de DesempeÒo', 'Variable', 'Activo'),
-('Vi·ticos', 'Variable', 'Activo'),
+('Bono de Desempe√±o', 'Variable', 'Activo'),
+('Vi√°ticos', 'Variable', 'Activo'),
 ('Pago por Proyecto', 'Variable', 'Activo'),
 ('Prima Vacacional', 'Fijo', 'Activo'),
-('GratificaciÛn', 'Variable', 'Activo'),
+('Gratificaci√≥n', 'Variable', 'Activo'),
 ('Honorarios', 'Fijo', 'Activo'),
 ('Pago Adicional', 'Variable', 'Activo'),
 ('Aguinaldo', 'Fijo', 'Activo'),
-('CompensaciÛn', 'Variable', 'Activo'),
+('Compensaci√≥n', 'Variable', 'Activo'),
 ('Otros Ingresos', 'Variable', 'Activo');
 
 SELECT * FROM [dbo].[Tipos de ingreso]
